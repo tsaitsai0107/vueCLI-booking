2a7669281a7b137d92d539b208b8478fc19f54aa
tsaitsai <vivian41521@yahoo.com.tw>
2021-02-26 01:12:59 +0800

    Fixed route

--- Vue.config.js
+++ Vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/vueCLI-booking/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/vuex-booking/" : "/",
 };
--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,12 @@
 //deploy.sh
-#!/usr/bin/env sh
-# 當發生錯誤時終止腳本運行
+
 set -e
-# 打包
 npm run build
-# 移動至到打包後的dist目錄 
 cd dist
-git init //因為dist資料夾預設是被ignore的，因此在進入dist資料夾後初始化git
+git init 
 git add -A
 git commit -m 'deploy'
 
-git push -f https://github.com/tsaitsai0107/vueCLI-booking.git master:gh-pages
+git push -f https://github.com/tsaitsai0107/vuex-booking.git master:gh-pages
 
 cd -
